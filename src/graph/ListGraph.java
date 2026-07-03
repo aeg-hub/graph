9843d5c6d342511ec9710d7d73613beb8ecdd692
Amro Homsani <amro.homsani@aeg-schueler.de>
2026-07-03 11:19:28 +0200

    getgrad

--- src/graph/ListGraph.java
+++ src/graph/ListGraph.java
@@ -354,6 +354,17 @@ public class ListGraph implements Graph {
 
         return result;
     }
+    
+    public int getGrad(Vertex pVertex) {
+    	int counter = 0;
+    	List<Vertex> neighbours = getNeighbours(pVertex);
+    	neighbours.toFirst();
+    	while(neighbours.hasAccess()) {
+    		counter++;
+    		neighbours.next();
+    	}
+    	return counter;
+    }
 
     /**
      * Liefert true, wenn der Graph keine Knoten enthaelt.
