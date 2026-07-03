3a8760eefa5abe6a985abdd3e6df7a53da2c26c8
Paul Rauen <paul.rauen@aeg-schueler.de>
2026-07-03 12:11:37 +0200

    FreundschaftsGraph implementation

--- src/beispiele/FreundschaftsGraph.java
+++ src/beispiele/FreundschaftsGraph.java
@@ -1,5 +1,6 @@
 package beispiele;
 
+import algorithm.Dijkstra;
 import graph.Edge;
 import graph.List;
 import graph.ListGraph;
@@ -32,6 +33,7 @@ public class FreundschaftsGraph {
         Edge KS = new Edge(Karl, Susi, 1);
         Edge KJ = new Edge(Karl, Jens, 1);
         Edge LM = new Edge(Lisa, Merle, 1);
+        Edge KL = new Edge(Karl, Lisa, 1);
                
 
         g.addEdge(SK);
@@ -41,8 +43,9 @@ public class FreundschaftsGraph {
         g.addEdge(KS);
         g.addEdge(KJ);
         g.addEdge(LM);
+        g.addEdge(KL);
         
-        
+     
         
         System.out.println("Knoten im Graphen:");
         List<Vertex> vertices = g.getVertices();
@@ -62,13 +65,32 @@ public class FreundschaftsGraph {
             edges.next();
         }
         
-  
+        System.out.println("\n" + vertexDegree(Karl, g));
+        
+        if (isInClique2(Lisa, Jens, g)) {
+        	System.out.println("\nJens ist in Lisas Clique");
+		}else {
+			System.out.println("\nJens ist nicht in Lisas Clique");
+		}
+        
+        System.out.println(isInClique2(Lisa, Jens, g) ? "true" : "false");
+        
+        Dijkstra dj = new Dijkstra();
+        dj.dijkstra(g, Merle);
+        
+        if (isInClique("Lisa", "Jens", g.getEdges())) {
+        	System.out.println("\nJens ist in Lisas Clique");
+		}else {
+			System.out.println("\nJens ist nicht in Lisas Clique");
+		}
 	}
+  
+	
 	
 	private static boolean isInClique(String von, String wer, List<Edge> edges) {
 		
 		edges.toFirst();
-		while (edges.hasAccess()) {
+		while (edges.hasAccess()) {			
 			if (edges.getContent().getVertices()[0].getID().equals(von) && edges.getContent().getVertices()[1].getID().equals(wer) || 
 					edges.getContent().getVertices()[1].getID().equals(wer) && edges.getContent().getVertices()[0].getID().equals(von) ) {
 				return true;
@@ -78,6 +100,38 @@ public class FreundschaftsGraph {
 		
 		return false;
 	}
+	
+	private static boolean isInClique2(Vertex von, Vertex wer, ListGraph g) {
+		
+		List<Vertex> nachbarn = g.getNeighbours(von);
+		
+		nachbarn.toFirst();
+		while (nachbarn.hasAccess()) {
+			if (nachbarn.getContent() == wer) {
+				return true;
+			}
+			nachbarn.next();
+		}
+		
+		
+		return false;
+	}
+	
+	private static int vertexDegree(Vertex v, ListGraph g) {
+		
+		List<Vertex> n = g.getNeighbours(v);
+		n.toFirst();
+		
+		int counter = 0;
+		
+		while (n.hasAccess()) {
+			counter++;
+			n.next();
+		}
+		
+		return counter;
+	}
 
 	
 }
+
